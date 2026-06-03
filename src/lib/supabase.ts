03c5ef2701ff48e7b0d8dca2b837e610e0516ee6
TheNest1234118 <nyma75738@gmail.com>
2026-06-03 09:23:23 +0200

    debug supabase url

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log("SUPABASE URL =", import.meta.env.VITE_SUPABASE_URL);
-console.log("SUPABASE KEY =", import.meta.env.VITE_SUPABASE_ANON_KEY);
+const url = import.meta.env.VITE_SUPABASE_URL;
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+console.log("SUPABASE URL =", url);
+
+export const supabase = createClient(url, key);
