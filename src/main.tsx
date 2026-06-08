bfb87e5faf1ef2e98b229240ceb4fc4a8ccc3191
TheNest1234118 <nyma75738@gmail.com>
2026-06-08 13:13:13 +0200

    qccaaaaaaads

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import OneSignal from "react-onesignal";
+
 OneSignal.init({
-    appId: "e92aa139-18cd-4df0-ab5b-138bfbbf57c4",
-    allowLocalhostAsSecureOrigin: true,
-  });
+  appId: "e92aa139-18cd-4df0-ab5b-138bfbbf57c4",
+  allowLocalhostAsSecureOrigin: true,
+  serviceWorkerPath: "/OneSignalSDKWorker.js",
+  serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
+});
+
 createRoot(document.getElementById("root")!).render(<App />);
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker.register("/sw.js").catch(console.error);
-    });
-  }
