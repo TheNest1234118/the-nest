ab5676a8ceee71619b09a3206936687b97b1366a
TheNest1234118 <nyma75738@gmail.com>
2026-06-13 18:53:46 +0200

    aaaar

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -6,9 +6,11 @@ const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
 console.log("SUPABASE URL =", url);
 
 const supabase = createClient(url, key, {
-    realtime: {
-      params: {
-        eventsPerSecond: 0,
-      },
+  realtime: {
+    params: {
+      eventsPerSecond: 0,
     },
-  });
+  },
+});
+
+export { supabase };
