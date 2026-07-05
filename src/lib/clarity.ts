8b0677ed4d74a1b9671fe27237c1fc3a534d2647
TheNest1234118 <nyma75738@gmail.com>
2026-07-05 11:17:58 +0200

    OGGGGGGGGFGGGGG

--- src/lib/clarity.ts
+++ src/lib/clarity.ts
@@ -1,12 +1,21 @@
 import Clarity from "@microsoft/clarity";
+import { supabase } from "@/lib/supabase";
 
-export function initClarity() {
+export async function initClarity() {
   const id = import.meta.env.VITE_CLARITY_ID;
-
-  console.log("CLARITY ID:", id);
 
   if (!id) return;
   if (typeof window === "undefined") return;
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // Dein eigener Account -> Clarity nicht starten
+  if (user?.id === "11171e8b-21a7-4258-b7f2-0a97e7d32e4d") {
+    console.log("Clarity disabled for owner");
+    return;
+  }
 
   Clarity.init(id);
 }
