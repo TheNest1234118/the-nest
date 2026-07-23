b5c5408628348d6e215b0d340d80ddcbb6015419
TheNest1234118 <nyma75738@gmail.com>
2026-07-23 05:36:25 +0200

    FIX

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-
   appId: 'com.thenest.app',
-
   appName: 'The Nest',
-
   webDir: 'dist/public',
-
 };
 
 export default config;
- 
